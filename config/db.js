03b4b307afe7ce674b9f9973ac894dcf494c289d
ghks6455 <ghks6455@gmail.com>
2024-12-09 16:56:01 +0900

    오탈자 수정

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWROD
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
 
 const db = pool.promise();
 
